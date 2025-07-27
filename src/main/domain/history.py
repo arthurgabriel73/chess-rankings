44f980504cd09124018d9d94f1077d27b070697d
arthurgabriel73 <arthurgabriel73@gmail.com>
2025-07-27 17:24:31 -0300

    feat: history

---
+++ src/main/domain/history.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+from typing import Dict, Optional
+
+@dataclass
+class History:
+    category: str
+    player_username: str
+    rating_history: Optional[Dict[str, int]]
+
+    def __post_init__(self):
+        if not self.rating_history:
+            self.rating_history = {}
