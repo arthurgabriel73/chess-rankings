06f21fd833ae3414b8d369bf27807584ecf67bf5
arthurgabriel73 <arthurgabriel73@gmail.com>
2025-07-27 19:16:27 -0300

    feat: LichessApiClient

---
+++ src/main/infra/adapter/driven/api/lichess/lichess_player_api_client.py
@@ -0,0 +1,20 @@
+import requests
+
+from src.main.infra.adapter.driven.api.dto.request.list_top_players_request import ListTopPlayersRequest
+from src.main.infra.adapter.driven.api.dto.response.list_top_players_response import ListTopPlayersResponse
+from src.main.infra.adapter.driven.api.player_api import PlayerApi
+from src.main.infra.config.environment_settings import get_environment_variables
+
+
+class LichessApiClient(PlayerApi):
+    def __init__(self, base_url: str = None):
+        env = get_environment_variables()
+        self._base_url = base_url or env.LICHESS_API_BASE_URL
+
+    def list_top_players(self, request: ListTopPlayersRequest) -> ListTopPlayersResponse:
+        category, num_players = request
+        response = requests.get(self._base_url + '/player' + f'/top/{num_players}/{category}')
+        if response.status_code != 200:
+            raise Exception(f'Failed to fetch top players: {response.status_code} - {response.text}')
+        data = response.json()
+        return ListTopPlayersResponse(data)
