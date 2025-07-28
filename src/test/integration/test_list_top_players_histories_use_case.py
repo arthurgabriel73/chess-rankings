453959041cd4efad7d9cf42221e2f1e557b9f77c
arthurgabriel73 <arthurgabriel73@gmail.com>
2025-07-28 18:51:46 -0300

    test: green test_should_raise_error_when_query_is_invalid

--- src/test/integration/test_list_top_players_histories_use_case.py
+++ src/test/integration/test_list_top_players_histories_use_case.py
@@ -33,3 +33,25 @@ class TestListTopPlayersHistoriesUseCase:
         assert len(output.histories) == num_players
         assert all(history['player'] == f'user{i}' for i, history in enumerate(output.histories))
         assert all(history['history'] == {f'{i}-07-01': i} for i, history in enumerate(output.histories))
+
+    @pytest.mark.parametrize(
+        'category, num_players, num_days, error_message',
+        [
+            (None, 5, 30, 'Invalid category'),
+            (7, 5, 30, 'Invalid category'),
+            ('', 5, 30, 'Invalid category'),
+            ('ab', 3, 30, 'Category must be between 3 and 100 characters long'),
+            ('a' * 101, 3, 30, 'Category must be between 3 and 100 characters long'),
+            ('bullet', '-1', 30, 'Number of players must be a positive integer and cannot exceed 50'),
+            ('bullet', 0, 30, 'Number of players must be a positive integer and cannot exceed 50'),
+            ('bullet', -1, 30, 'Number of players must be a positive integer and cannot exceed 50'),
+            ('bullet', 51, 30, 'Number of players must be a positive integer and cannot exceed 50'),
+            ('bullet', 5, -1, 'Number of days must be a positive integer'),
+        ],
+    )
+    def test_should_raise_error_when_query_is_invalid(
+        self, setup, category: str, num_players: int, num_days: int, error_message: str
+    ):
+        # Act & Assert
+        with pytest.raises(ValueError, match=error_message):
+            ListTopPlayersHistoriesQuery(category=category, num_players=num_players, num_days=num_days)
