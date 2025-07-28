982e7c56508bebf33b4541f523cc9d20ae86f689
arthurgabriel73 <arthurgabriel73@gmail.com>
2025-07-28 18:02:07 -0300

    refactor: uses ProcessedPlayerHistory in ListRatingHistoriesResponseAdapter

--- src/main/infra/adapter/driven/api/lichess/adapter/response/get_rating_histories_response_adapter.py
+++ src/main/infra/adapter/driven/api/lichess/adapter/response/get_rating_histories_response_adapter.py
@@ -1,10 +1,11 @@
-from typing import Dict, List, Union
+from typing import List
 
 from src.main.domain.history import History
+from src.main.infra.adapter.driven.api.lichess.processed_player_history import ProcessedPlayerHistory
 
 
 class ListRatingHistoriesResponseAdapter:
-    def __init__(self, category: str, rating_histories: List[Dict[str, Union[str, Dict]]]):
+    def __init__(self, category: str, rating_histories: List[ProcessedPlayerHistory]):
         self.category = category
         self.rating_histories = rating_histories
 
