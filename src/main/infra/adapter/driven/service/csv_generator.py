7ef96e18f37bf0dd94f41d03f0cb4773f954970f
arthurgabriel73 <arthurgabriel73@gmail.com>
2025-07-28 19:46:45 -0300

    wip: FileGeneratorServiceGateway

--- src/main/infra/adapter/driven/service/csv_generator.py
+++ src/main/infra/adapter/driven/service/csv_generator.py
@@ -1,2 +1,19 @@
+import csv
+from io import StringIO
+from typing import List
+
+
 class CSVGenerator:
-    pass
+    @staticmethod
+    def generate_csv(headers: List, rows: List, delimiter: str) -> bytes:
+        CSVGenerator._validate_delimiter(delimiter)
+        output = StringIO()
+        writer = csv.writer(output, delimiter=delimiter)
+        writer.writerow(headers)
+        writer.writerows(rows)
+        return output.getvalue().encode('utf-8')
+
+    @staticmethod
+    def _validate_delimiter(delimiter: str) -> None:
+        if delimiter not in [',', ';', '|']:
+            raise ValueError(f"Invalid delimiter: {delimiter}. Supported delimiters are: ',', ';', '|'")
--- src/main/infra/adapter/driven/service/file_generator_service_gateway.py
+++ src/main/infra/adapter/driven/service/file_generator_service_gateway.py
@@ -10,4 +10,12 @@ class FileGeneratorServiceGateway(FileGeneratorService):
         self._csv_generator = csv_generator
 
     def generate_history_file(self, histories: List[History]) -> bytes:
+        headers = self._generate_history_headers(histories)
+        rows = self._generate_history_rows(histories)
+        return self._csv_generator.generate_csv(headers, rows, delimiter=',')
+
+    def _generate_history_headers(self, histories: List[History]) -> List[str]:
+        pass
+
+    def _generate_history_rows(self, histories: List[History]) -> List[str]:
         pass
