e48cac873a251f662dcc945c6f43a581dd417445
arthurgabriel73 <arthurgabriel73@gmail.com>
2025-07-29 18:32:52 -0300

    wip: mock server

--- src/test/resources/json_data/top_players_rating_histories.py
+++ src/test/resources/json_data/top_players_rating_histories.py
@@ -1,15 +1,26 @@
+import random
+from datetime import datetime, timedelta
+
+
 def get_player_rating_history_test_response(username: str) -> list:
+    today = datetime.now()
+    offsets_in_days = list(range(29, -1, -1))
+    initial_rating_for_oldest_day = 1500
+    rating_increase_per_day = 17
+
+    dynamic_points = []
+    for i, days_ago in enumerate(offsets_in_days):
+        past_date = today - timedelta(days=days_ago * 2)
+        current_day_rating = initial_rating_for_oldest_day + i * rating_increase_per_day
+        current_day_rating += random.randint(-5, 5)
+        current_day_rating = max(1400, current_day_rating)
+
+        dynamic_points.append([past_date.year, past_date.month, past_date.day, current_day_rating])
+
     return [
-        {
-            'name': 'UltraBullet',
-            'points': [[2020, 8, 15, 1577], [2020, 9, 6, 1574], [2022, 1, 14, 1510], [2023, 6, 17, 1508]],
-        },
-        {
-            'name': 'Bullet',
-            'points': [[2020, 8, 15, 1577], [2020, 9, 6, 1574], [2022, 1, 14, 1510], [2023, 6, 17, 1508]],
-        },
-        {
-            'name': 'Classical',
-            'points': [[2020, 8, 15, 1577], [2020, 9, 6, 1574], [2022, 1, 14, 1510], [2023, 6, 17, 1508]],
-        },
+        {'name': 'UltraBullet', 'points': dynamic_points},
+        {'name': 'Bullet', 'points': dynamic_points},
+        {'name': 'Classical', 'points': dynamic_points},
+        {'name': 'Blitz', 'points': dynamic_points},
+        {'name': 'Rapid', 'points': dynamic_points},
     ]
