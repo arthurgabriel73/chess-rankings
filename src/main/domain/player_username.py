d7862ad91af65d22e32332263e1346f8d4053b61
arthurgabriel73 <arthurgabriel73@gmail.com>
2025-07-28 22:29:37 -0300

    refactor: change error type

--- src/main/domain/player_username.py
+++ src/main/domain/player_username.py
@@ -14,7 +14,7 @@ class PlayerUsername:
         if not self._value:
             raise ValueError('Username cannot be null or empty')
         if not isinstance(self._value, str):
-            raise TypeError('Username must be a string')
+            raise ValueError('Username must be a string')
         if len(self._value) < self._MIN_LENGTH or len(self._value) > self._MAX_LENGTH:
             raise ValueError(f'Username must be between {self._MIN_LENGTH} and {self._MAX_LENGTH} characters long')
 
