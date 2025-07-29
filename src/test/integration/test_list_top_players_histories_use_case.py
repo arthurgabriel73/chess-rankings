d2a459d09ebd264698ba0df387c26d6d2132a6ee
arthurgabriel73 <arthurgabriel73@gmail.com>
2025-07-28 23:18:01 -0300

    test: fixes TestListTopPlayersHistoriesUseCase

--- src/test/integration/test_list_top_players_histories_use_case.py
+++ src/test/integration/test_list_top_players_histories_use_case.py
@@ -2,6 +2,7 @@ import pytest
 
 from src.main.application.port.driven.api.player_gateway import PlayerGateway
 from src.main.application.port.driven.ui.ui_handler import UIHandler
+from src.main.application.port.driver.model.category_validator import CategoryValidator
 from src.main.application.port.driver.model.query.list_top_players_histories_query import ListTopPlayersHistoriesQuery
 from src.main.application.usecase.list_top_players_histories_use_case import ListTopPlayersHistoriesUseCase
 from src.test.integration.mock.mock_player_gateway import MockPlayerGateway
@@ -37,11 +38,11 @@ class TestListTopPlayersHistoriesUseCase:
     @pytest.mark.parametrize(
         'category, num_players, num_days, error_message',
         [
-            (None, 5, 30, 'Invalid category'),
-            (7, 5, 30, 'Invalid category'),
-            ('', 5, 30, 'Invalid category'),
-            ('ab', 3, 30, 'Category must be between 3 and 100 characters long'),
-            ('a' * 101, 3, 30, 'Category must be between 3 and 100 characters long'),
+            (None, 5, 30, f'Invalid category: must be one of: {", ".join(CategoryValidator.valid_categories)}'),
+            (7, 5, 30, f'Invalid category: must be one of: {", ".join(CategoryValidator.valid_categories)}'),
+            ('', 5, 30, f'Invalid category: must be one of: {", ".join(CategoryValidator.valid_categories)}'),
+            ('ab', 3, 30, f'Invalid category: must be one of: {", ".join(CategoryValidator.valid_categories)}'),
+            ('a' * 101, 3, 30, f'Invalid category: must be one of: {", ".join(CategoryValidator.valid_categories)}'),
             ('bullet', '-1', 30, 'Number of players must be a positive integer and cannot exceed 50'),
             ('bullet', 0, 30, 'Number of players must be a positive integer and cannot exceed 50'),
             ('bullet', -1, 30, 'Number of players must be a positive integer and cannot exceed 50'),
